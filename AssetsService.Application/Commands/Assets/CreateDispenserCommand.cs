9bb4cf9f6f829f3454e16d38dac938cd614fdc73
adarsh.singh <adarsh.singh@LT1938>
2026-02-09 16:30:18 +0530

    ICCID Character Limit

--- AssetsService.Application/Commands/Assets/CreateDispenserCommand.cs
+++ AssetsService.Application/Commands/Assets/CreateDispenserCommand.cs
@@ -1,13 +1,6 @@
 using AssetsService.Application.Responses.Assets;
-using AssetsService.Core.Entities;
-using AssetsService.Core.Responses;
 using MediatR;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AssetsService.Application.Commands.Assets
 {
@@ -15,22 +8,22 @@ namespace AssetsService.Application.Commands.Assets
     {
         [Required]
         [StringLength(20, MinimumLength = 0, ErrorMessage = "AssetId must be fewer than 20 characters.")]
-        public string AssetId { get; set; }
+        public string AssetId { get; set; } = string.Empty;
         [Required]
         [StringLength(100, MinimumLength = 0, ErrorMessage = "ChargeBoxId must be fewer than 100 characters.")]
-        public string ChargeBoxId { get; set; }
-        
-        [StringLength(15, MinimumLength = 0, ErrorMessage = "SimCardMSIDN must be fewer than 15 characters.")]
-        public string? SimCardMSIDN { get; set; }   
+        public string ChargeBoxId { get; set; } = string.Empty;
+
+        [StringLength(20, MinimumLength = 0, ErrorMessage = "SimCardMSIDN must be fewer than 20 characters")]
+        public string? SimCardMSIDN { get; set; }
         public long? ModemId { get; set; } = 0;    // 07/11/2022
         [StringLength(20, MinimumLength = 0, ErrorMessage = "MeterType must be fewer than 20 characters.")]
-        public string MeterType { get; set; }
+        public string MeterType { get; set; } = string.Empty;
         [StringLength(20, MinimumLength = 0, ErrorMessage = "PingSchedule must be fewer than 20 characters.")]
-        public string PingSchedule { get; set; }
+        public string PingSchedule { get; set; } = string.Empty;
         [Required]
         public bool FleetStation { get; set; }        // PrivateStation changed to FleetStation  07/11/2022
         [StringLength(20, MinimumLength = 0, ErrorMessage = "ReadingSchedule must be fewer than 20 characters.")]
-        public string ReadingSchedule { get; set; }
+        public string ReadingSchedule { get; set; } = string.Empty;
         public long? RFIdReaderId { get; set; } = 0;
 
         public long? PowerCabinetId { get; set; } = 0;
@@ -42,9 +35,9 @@ namespace AssetsService.Application.Commands.Assets
 
         [Required]
         [StringLength(20, MinimumLength = 0, ErrorMessage = "ProtocolName must be fewer than 20 characters.")]
-        public string ProtocolName { get; set; }
+        public string ProtocolName { get; set; } = string.Empty;
         [Required]
-        public string CreatedBy { get; set; }
+        public string CreatedBy { get; set; } = string.Empty;
         [Required]
         public bool IsActive { get; set; }
         public DateTime InstallationDate { get; set; }
@@ -61,29 +54,29 @@ namespace AssetsService.Application.Commands.Assets
         public long PlugTypeId { get; set; }
         [Required]
         [StringLength(20, ErrorMessage = "PortName must be fewer than 20 characters.")]
-        public string PortName { get; set; }
+        public string PortName { get; set; } = string.Empty;
         [Required]
-        public string Power { get; set; }
+        public string Power { get; set; } = string.Empty;
     }
 
     public abstract class DispenserCommandBase
     {
         [StringLength(20, MinimumLength = 0, ErrorMessage = "EndPointUrl must be fewer than 20 characters.")]
-        public string EndPointUrl { get; set; }
+        public string EndPointUrl { get; set; } = string.Empty;
         [StringLength(20, MinimumLength = 0, ErrorMessage = "FirmwareVersion must be fewer than 20 characters.")]
-        public string FirmwareVersion { get; set; }
+        public string FirmwareVersion { get; set; } = string.Empty;
         [Required]
         [StringLength(100, MinimumLength = 0, ErrorMessage = "HardwareSerialNumber must be fewer than 100 characters.")]
-        public string HardwareSerialNumber { get; set; }
+        public string HardwareSerialNumber { get; set; } = string.Empty;
         [Required]
         [Range(1, long.MaxValue, ErrorMessage = "Please enter valid Location Id")]
         public long LocationId { get; set; }
         [Required]
         [RegularExpression("^[a-zA-Z0-9 \\-]{0,40}$", ErrorMessage = "Only Alphabets and Numbers allowed.")]
-        public string MakeName { get; set; }
+        public string MakeName { get; set; } = string.Empty;
         [Required]
         [RegularExpression("^[a-zA-Z0-9 \\-]{0,40}$", ErrorMessage = "Only Alphabets and Numbers allowed.")]
-        public string ModelName { get; set; }
+        public string ModelName { get; set; } = string.Empty;
         public double? Longitude { get; set; }
         public double? Latitude { get; set; }
     }
@@ -97,13 +90,13 @@ namespace AssetsService.Application.Commands.Assets
         [Range(1, long.MaxValue, ErrorMessage = "Please enter valid ConnectorType Id")]
         public long ConnectorType { get; set; }
         [Required]
-        public string IncrementalPower { get; set; }
+        public string IncrementalPower { get; set; } = string.Empty;
         [Required]
         public bool IsActive { get; set; }
         [Required]
-        public string MaxPower { get; set; }
+        public string MaxPower { get; set; } = string.Empty;
         [Required]
-        public string MinPower { get; set; }
-        
+        public string MinPower { get; set; } = string.Empty;
+
     }
 }
--- AssetsService.Application/Commands/Assets/UpdateDispenserCommand.cs
+++ AssetsService.Application/Commands/Assets/UpdateDispenserCommand.cs
@@ -1,5 +1,4 @@
 using AssetsService.Application.Responses.Assets;
-using AssetsService.Core.Responses;
 using MediatR;
 using System.ComponentModel.DataAnnotations;
 
@@ -13,27 +12,23 @@ namespace AssetsService.Application.Commands.Assets
         public int Id { get; set; }
         [Required]
         [StringLength(20, MinimumLength = 0, ErrorMessage = "AssetId must be fewer than 20 characters.")]
-        public string AssetId { get; set; }
+        public string AssetId { get; set; } = string.Empty;
 
-        [StringLength(15, MinimumLength = 0, ErrorMessage = "SimCardMSIDN must be fewer than 15 characters.")]
+        [StringLength(20, MinimumLength = 0, ErrorMessage = "SimCardMSIDN must be fewer than 20 characters")]
         public string? SimCardMSIDN { get; set; }
         [Required]
         [StringLength(100, MinimumLength = 0, ErrorMessage = "ChargeBoxId must be fewer than 100 characters.")]
-        public string ChargeBoxId { get; set; } 
+        public string ChargeBoxId { get; set; } = string.Empty;
         public long? ModemId { get; set; }
         [StringLength(20, MinimumLength = 0, ErrorMessage = "MeterType must be fewer than 20 characters.")]
-        public string MeterType { get; set; }
+        public string MeterType { get; set; } = string.Empty;
 
         [StringLength(20, MinimumLength = 0, ErrorMessage = "PingSchedule must be fewer than 20 characters.")]
-        public string PingSchedule { get; set; }
+        public string PingSchedule { get; set; } = string.Empty;
         [Required]
         public bool FleetStation { get; set; }
         [StringLength(20, MinimumLength = 0, ErrorMessage = "ReadingSchedule must be fewer than 20 characters.")]
-        public string ReadingSchedule { get; set; }
-        //[Required]
-        //[StringLength(20, MinimumLength = 0, ErrorMessage = "SerialNumber must be fewer than 20 characters.")]
-        //public string SerialNumber { get; set; }          // Removed  11/07/2022
-
+        public string ReadingSchedule { get; set; } = string.Empty;
         public long? RFIdReaderId { get; set; }
 
         public long? PowerCabinetId { get; set; }
@@ -46,9 +41,9 @@ namespace AssetsService.Application.Commands.Assets
 
         [Required]
         [StringLength(20, MinimumLength = 0, ErrorMessage = "ProtocolName must be fewer than 20 characters.")]
-        public string ProtocolName { get; set; }
+        public string ProtocolName { get; set; } = string.Empty;
         [Required]
-        public string ModifiedBy { get; set; }
+        public string ModifiedBy { get; set; } = string.Empty;
         [Required]
         public bool IsActive { get; set; }
         public DateTime InstallationDate { get; set; }
@@ -58,17 +53,17 @@ namespace AssetsService.Application.Commands.Assets
         [Required]
         public List<UpdatePortCommand>? UpdatePortCommand { get; set; }
     }
-    public class UpdatePortCommand: PortCommandBase
+    public class UpdatePortCommand : PortCommandBase
     {
-        public long Id { get; set; } 
-        
+        public long Id { get; set; }
+
         [Required]
         [Range(1, long.MaxValue, ErrorMessage = "Please enter valid PlugType Id")]
         public long PlugTypeId { get; set; }
         [Required]
         [StringLength(20, ErrorMessage = "PortName must be fewer than 20 characters.")]
-        public string PortName { get; set; }
+        public string PortName { get; set; } = string.Empty;
         [Required]
-        public string Power { get; set; }
+        public string Power { get; set; } = string.Empty;
     }
 }
